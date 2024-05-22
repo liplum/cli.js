581a50439e0b1c50ba4ad73da039a2bf6e87d128
Liplum <Li_plum@outlook.com>
2024-05-23 03:08:50 +0800

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,472 @@
+import commandLineArgs from 'command-line-args';
+import commandLineUsage from 'command-line-usage';
+import removeMarkdown from 'remove-markdown';
+import didYouMean from "didyoumean"
+import chalk from 'chalk';
+
+/** An options for a CLI command */
+export type Option = commandLineUsage.OptionDefinition;
+
+/** A detailed example of the usage of a CLI command */
+export type Example = {
+  /** A description of what the example is showcasing */
+  desc: string;
+  /** The actual example */
+  example: string;
+};
+
+export type Section = commandLineUsage.Section & {
+  /** Whether the body of the section should be formatted like a code block */
+  code?: boolean;
+};
+
+/** A command for a CLI */
+export type Command = {
+  /** The name of the CLI command. Used when running in a terminal */
+  name: string;
+  /** A description of what the command does */
+  description: string;
+  /** Options that the command can accept */
+  options?: Option[];
+  /**
+   * A list of what options are required.
+   * You can also have 1 of multiple required options.
+   * In the following example c is always required and a or b is required.
+   *
+   * @example
+   * {
+   *   require: [['a', 'b'], 'c']
+   * }
+   */
+  require?: (string | string[] | (string | string[])[])[];
+  /** Examples showcasing common usage of the command */
+  examples?: (string | Example)[];
+  /** What group to render the command in a MultiCommand */
+  group?: string;
+  /** Extra info and documentation about a command */
+  footer?: Section[] | Section | string;
+};
+
+/** A command that is a collection of sub-command */
+export interface MultiCommand {
+  /** The name of the multi command. Used when running in a terminal */
+  name: string;
+  /** An optional logo to display above the help text */
+  logo?: string;
+  /** A description of what the command does */
+  description: string;
+  /** A list of options that will be available to all sub-commands */
+  options?: Option[];
+  /** The sub-commands for the multi command */
+  commands: (Command | MultiCommand)[];
+  /** Extra info and documentation about a command */
+  footer?: Section | string;
+}
+
+const help: Option = {
+  name: 'help',
+  alias: 'h',
+  description: 'Display the help output',
+  type: Boolean,
+  group: 'global'
+};
+
+/** Options available to all CLIs created with this tool */
+export const globalOptions = [help];
+
+const arrayify = <T>(x: T | T[]): T[] => (Array.isArray(x) ? x : [x]);
+const hasGlobal = (options: Option[]) =>
+  Boolean(options.find(option => option.group === 'global'));
+
+function styleTypes(command: Command, option: Option) {
+  const isRequired = command.require && command.require.includes(option.name);
+
+  if (isRequired && option.type === Number) {
+    option.typeLabel = `{rgb(173, 216, 230) {underline ${option.typeLabel ||
+      'number'}}} [{rgb(254,91,92) required}]`;
+  } else if (option.type === Number) {
+    option.typeLabel = `{rgb(173, 216, 230) {underline ${option.typeLabel ||
+      'number'}}}`;
+  }
+
+  if (isRequired && option.type === String) {
+    option.typeLabel = `{rgb(173, 216, 230) {underline ${option.typeLabel ||
+      'string'}}} [{rgb(254,91,92) required}]`;
+  } else if (option.multiple && option.type === String) {
+    option.typeLabel = `{rgb(173, 216, 230) {underline ${option.typeLabel ||
+      'string[]'}}}`;
+  } else if (option.type === String) {
+    option.typeLabel = `{rgb(173, 216, 230) {underline ${option.typeLabel ||
+      'string'}}}`;
+  }
+}
+
+function addFooter(command: Command, sections: Section[]) {
+  if (typeof command.footer === 'string') {
+    sections.push({ content: command.footer });
+  } else if (command.footer) {
+    const footers = Array.isArray(command.footer)
+      ? command.footer
+      : [command.footer];
+
+    footers.forEach(f => {
+      let content = !('content' in f)
+        ? undefined
+        : typeof f.content === 'string'
+          ? removeMarkdown(
+            f.code
+              ? f.content
+                .split('\n')
+                .map(s => (s.includes('```') ? undefined : `| ${s}`))
+                .join('\n')
+              : f.content,
+            {
+              stripListLeaders: false
+            }
+          )
+          : Array.isArray(f.content)
+            ? f.content
+            : undefined;
+
+      if (typeof content === 'string') {
+        content = content.replace(/}/g, '\\}').replace(/{/g, '\\{');
+      }
+
+      sections.push({
+        ...f,
+        header: f.header
+          ? removeMarkdown(f.header, { stripListLeaders: false })
+          : undefined,
+        content
+      });
+    });
+  }
+}
+
+const printUsage = (command: Command) => {
+  const options = command.options || [];
+  const sections: Section[] = [
+    {
+      header: command.name,
+      content: command.description
+    }
+  ];
+
+  options.forEach(option => {
+    styleTypes(command, option);
+  });
+
+  if (hasGlobal(options)) {
+    sections.push(
+      {
+        header: 'Options',
+        optionList: options.filter(o => o.group !== 'global')
+      },
+      {
+        header: 'Global Options',
+        optionList: [...options, ...globalOptions],
+        group: 'global'
+      }
+    );
+  } else {
+    sections.push({
+      header: 'Options',
+      optionList: [...options, ...globalOptions]
+    });
+  }
+
+  if (command.examples) {
+    sections.push({
+      header: 'Examples',
+      content: command.examples
+    });
+  }
+
+  addFooter(command, sections);
+
+  console.log(commandLineUsage(sections));
+  return;
+};
+
+const printRootUsage = (multi: MultiCommand) => {
+  const subCommands =
+    multi.commands.filter((c): c is Command => !('isMulti' in c)) || [];
+  const rootOptions = multi.options || [];
+  const options = [...rootOptions, ...globalOptions];
+  const sections: Section[] = [];
+
+  if (multi.logo) {
+    sections.push({
+      content: multi.logo,
+      raw: true
+    });
+  }
+
+  sections.push({
+    header: multi.name,
+    content: multi.description
+  });
+
+  sections.push({
+    header: 'Synopsis',
+    content: `$ ${multi.name} <command> <options>`
+  });
+
+  const groups = subCommands.reduce((all, command) => {
+    if (command.group) {
+      all.add(command.group);
+    }
+
+    return all;
+  }, new Set<string>());
+
+  groups.forEach(header => {
+    const grouped = subCommands.filter(c => c.group === header) || [];
+
+    if (grouped.length > 0) {
+      sections.push({
+        header,
+        content: grouped.map(command => ({
+          name: command.name,
+          description: command.description
+        }))
+      });
+    }
+  });
+
+  if (groups.size === 0) {
+    sections.push({
+      header: 'Commands',
+      content: subCommands.map(command => ({
+        name: command.name,
+        description: command.description
+      }))
+    });
+  }
+
+  options.forEach(option => {
+    styleTypes(multi, option);
+  });
+
+  sections.push({
+    header: 'Global Options',
+    optionList: options,
+    group: ['_none', 'global']
+  });
+
+  addFooter(multi, sections);
+
+  console.log(commandLineUsage(sections));
+};
+
+const errorReportingStyles = ['exit', 'throw', 'object'] as const;
+type ErrorReportingStyle = typeof errorReportingStyles[number];
+
+const reportError = (error: string, style: ErrorReportingStyle) => {
+  if (style === 'exit') {
+    console.log(error);
+    process.exit(1);
+  }
+
+  if (style === 'throw') {
+    throw new Error(error);
+  }
+
+  if (style === 'object') {
+    return { error };
+  }
+
+  return;
+};
+
+const createList = (list: string[], transform: (value: string) => string) => {
+  const [first, ...rest] = list.map(transform);
+  return rest.length > 0 ? `${rest.join(', ')} or ${first}` : first;
+};
+
+const reportUnknownFlags = (
+  args: (Option | Command)[],
+  [unknown]: string[],
+  errorStyle: ErrorReportingStyle
+) => {
+  const unknownStyled = chalk.redBright(`"${unknown}"`);
+  const type = unknown.startsWith('-') ? 'flag' : 'command';
+  let suggestions = didYouMean(
+    unknown,
+    args.map(a => (type === 'flag' ? `--${a.name}` : a.name))
+  );
+
+  let error: string;
+  suggestions = Array.isArray(suggestions) ? suggestions : [suggestions]
+  if (suggestions.length) {
+    const list = createList(suggestions, s => chalk.greenBright(`"${s}"`));
+    error = `Found unknown ${type} ${unknownStyled}, did you mean ${list}?`;
+  } else {
+    error = `Found unknown ${type}: ${unknownStyled}`;
+  }
+
+  return reportError(error, errorStyle);
+};
+
+const initializeOptions = (options: Option[] = []) => {
+  const args = [...options];
+
+  globalOptions.forEach(o => {
+    if (!args.find(a => a.name === o.name)) {
+      args.push(o);
+    }
+  });
+
+  return args;
+};
+
+interface Options {
+  /** Override the agreements that command-line-application parses */
+  argv?: string[];
+  /** Configure whether command-line-application shows the help prompt */
+  showHelp?: boolean;
+  /** Control how command-line-application reports errors */
+  error?: ErrorReportingStyle;
+  /** Convert parsed options to camelCase */
+  camelCase?: boolean;
+}
+
+const parseCommand = (
+  command: Command,
+  { argv, showHelp, error = 'exit', camelCase = true }: Options
+): Record<string, any> | undefined => {
+  const args = initializeOptions(command.options);
+  const { global, ...rest } = commandLineArgs(args, {
+    stopAtFirstUnknown: true,
+    camelCase,
+    argv
+  });
+
+  if (rest._unknown) {
+    printUsage(command);
+    return reportUnknownFlags(args, rest._unknown, error);
+  }
+
+  if (global.help && showHelp) {
+    printUsage(command);
+    return;
+  }
+
+  const formatArrayOption = (option: any): string =>
+    typeof option === 'string'
+      ? `--${option}`
+      : option.map(formatArrayOption).join(', ');
+
+  if (command.require) {
+    const missing = command.require
+      .filter(
+        option =>
+          (typeof option === 'string' && !(option in rest._all)) ||
+          (typeof option === 'object' &&
+            !option.find(
+              o =>
+                (typeof o === 'string' && o in rest._all) ||
+                (typeof o === 'object' && !o.find(op => !(op in rest._all)))
+            )) ||
+          // tslint:disable-next-line strict-type-predicates
+          (typeof option === 'string' && rest._all[option] === null)
+      )
+      .map(option =>
+        typeof option === 'string'
+          ? `--${option}`
+          : `(${(option as any[]).map(formatArrayOption).join(' or ')})`
+      );
+
+    if (missing.length > 0) {
+      const multiple = missing.length > 1;
+      printUsage(command);
+
+      return reportError(
+        `Missing required arg${multiple ? 's' : ''}: ${missing.join(', ')}`,
+        error
+      );
+    }
+  }
+
+  return { ...rest, ...rest._all, ...global };
+};
+
+/**
+ * Create a command line application with all the bells and whistles
+ * @param command the command to create an application for
+ * @param options Advanced options for the application
+ */
+export function app(
+  command: Command | MultiCommand,
+  { showHelp = true, argv, error = 'exit', camelCase = true }: Options = {}
+):
+  | (({ _command: string | string[] } | { error: string } | {}) &
+    Record<string, any>)
+  | undefined {
+  const appOptions = { showHelp, argv, error, camelCase };
+
+  if (!('commands' in command)) {
+    return parseCommand(command, appOptions);
+  }
+
+  const rootOptions = initializeOptions(command.options);
+  const { global, _unknown, _all } = commandLineArgs(rootOptions, {
+    stopAtFirstUnknown: true,
+    camelCase,
+    argv
+  });
+
+  if (global.help && showHelp) {
+    printRootUsage(command);
+    return;
+  }
+
+  if (_unknown && _unknown.length > 0) {
+    const subCommand = command.commands.find((c): c is Command =>
+      Boolean(c.name === _unknown[0])
+    );
+
+    if (subCommand) {
+      const options = [
+        ...(subCommand.options || []),
+        ...(command.options || [])
+      ];
+      const parsed = app(
+        { ...subCommand, options },
+        { ...appOptions, argv: _unknown.slice(1) }
+      );
+
+      if (!parsed) {
+        return;
+      }
+
+      return {
+        ...parsed,
+        _command:
+          '_command' in parsed
+            ? [subCommand.name, ...arrayify(parsed._command)]
+            : subCommand.name
+      };
+    }
+
+    printRootUsage(command);
+    return reportUnknownFlags(
+      [...rootOptions, ...command.commands],
+      _unknown,
+      error
+    );
+  }
+
+  if (Object.keys(_all).length > 0) {
+    return _all;
+  }
+
+  if (showHelp) {
+    printRootUsage(command);
+  }
+
+  return reportError(
+    `No sub-command provided to MultiCommand "${command.name}"`,
+    error
+  );
+}
---
+++ test/index.test.ts
@@ -0,0 +1,554 @@
+import { app, Command, MultiCommand } from '../src';
+import stripAnsi from 'strip-ansi';
+
+beforeEach(() => {
+  process.argv = [];
+  jest.clearAllMocks();
+});
+
+describe('single command app', () => {
+  const echo: Command = {
+    name: 'echo',
+    description: 'Print a string to the terminal',
+    examples: ['echo foo', 'echo "Intense message"'],
+    options: [
+      {
+        name: 'value',
+        type: String,
+        defaultOption: true,
+        description: 'The value to print'
+      }
+    ]
+  };
+
+  test('usage', () => {
+    expect(app(echo, { argv: ['foo'] })).toEqual(
+      expect.objectContaining({ value: 'foo' })
+    );
+  });
+
+  test('help', () => {
+    jest.spyOn(console, 'log').mockImplementationOnce(() => { });
+    app(echo, { argv: ['--help'] });
+
+    // @ts-ignore
+    expect(console.log.mock.calls[0]).toMatchSnapshot();
+  });
+
+  test('errors on unknown flag', () => {
+    jest.spyOn(console, 'log').mockImplementation(() => { });
+    // @ts-ignore
+    jest.spyOn(process, 'exit').mockImplementationOnce(() => { });
+
+    app(echo, { argv: ['--error'] });
+
+    // @ts-ignore
+    expect(console.log.mock.calls[0]).toMatchSnapshot();
+    // @ts-ignore
+    expect(console.log.mock.calls[1]).toMatchSnapshot();
+    // @ts-ignore
+    expect(process.exit).toHaveBeenCalledWith(1);
+  });
+
+  test('errors on unknown flag + known flags', () => {
+    jest.spyOn(console, 'log').mockImplementation(() => { });
+    // @ts-ignore
+    jest.spyOn(process, 'exit').mockImplementationOnce(() => { });
+
+    app(echo, { argv: ['--error', '--value=foo'] });
+
+    // @ts-ignore
+    expect(console.log.mock.calls[1]).toMatchSnapshot();
+    // @ts-ignore
+    expect(process.exit).toHaveBeenCalledWith(1);
+  });
+
+  test('suggests typo correction', () => {
+    // @ts-ignore
+    jest.spyOn(process, 'exit').mockImplementationOnce(() => { });
+    jest.spyOn(console, 'log').mockImplementation(() => { });
+
+    app(echo, { argv: ['--vakue', 'foo'] });
+
+    // @ts-ignore
+    expect(console.log.mock.calls[0]).toMatchSnapshot();
+    // @ts-ignore
+    expect(console.log.mock.calls[1]).toMatchSnapshot();
+    // @ts-ignore
+    expect(process.exit).toHaveBeenCalledWith(1);
+  });
+
+  test('can configure error type to throw', () => {
+    // @ts-ignore
+    jest.spyOn(process, 'exit').mockImplementationOnce(() => { });
+    jest.spyOn(console, 'log').mockImplementation(() => { });
+
+    expect(() =>
+      app(echo, { argv: ['--vakue', 'foo'], error: 'throw' })
+    ).toThrow();
+  });
+
+  test('can configure error type to return object', () => {
+    // @ts-ignore
+    jest.spyOn(process, 'exit').mockImplementationOnce(() => { });
+    jest.spyOn(console, 'log').mockImplementation(() => { });
+
+    expect(
+      stripAnsi(app(echo, { argv: ['--vakue', 'foo'], error: 'object' })!.error)
+    ).toBe('Found unknown flag "--vakue", did you mean "--value"?');
+  });
+
+  test('errors without required flag', () => {
+    jest.spyOn(console, 'log').mockImplementation(() => { });
+    // @ts-ignore
+    jest.spyOn(process, 'exit').mockImplementationOnce(() => { });
+
+    app({ ...echo, require: ['value'] }, { argv: [] });
+
+    // @ts-ignore
+    expect(console.log.mock.calls[0]).toMatchSnapshot();
+    // @ts-ignore
+    expect(console.log.mock.calls[1]).toMatchSnapshot();
+    // @ts-ignore
+    expect(process.exit).toHaveBeenCalledWith(1);
+  });
+
+  test('errors without required flag - or', () => {
+    jest.spyOn(console, 'log').mockImplementation(() => { });
+    // @ts-ignore
+    jest.spyOn(process, 'exit').mockImplementationOnce(() => { });
+
+    app({ ...echo, require: [['a', 'b'], 'c'] }, { argv: [] });
+
+    // @ts-ignore
+    expect(console.log.mock.calls[0]).toMatchSnapshot();
+    // @ts-ignore
+    expect(console.log.mock.calls[1]).toMatchSnapshot();
+    // @ts-ignore
+    expect(process.exit).toHaveBeenCalledWith(1);
+  });
+
+  test('errors without multiple required flags - or', () => {
+    jest.spyOn(console, 'log').mockImplementation(() => { });
+    // @ts-ignore
+    jest.spyOn(process, 'exit').mockImplementationOnce(() => { });
+
+    app(
+      {
+        ...echo,
+        options: [
+          { name: 'b', description: '' },
+          { name: 'c', description: '' }
+        ],
+        require: [['a', ['b', 'c']]]
+      },
+      { argv: ['--b', 'b'] }
+    );
+
+    app(
+      {
+        ...echo,
+        options: [
+          { name: 'b', description: '' },
+          { name: 'c', description: '' }
+        ],
+        require: [['a', ['b', 'c']]]
+      },
+      { argv: ['--b', 'b', '--c', 'c'] }
+    );
+
+    // @ts-ignore
+    expect(console.log.mock.calls[0]).toMatchSnapshot();
+    // @ts-ignore
+    expect(console.log.mock.calls[1]).toMatchSnapshot();
+    // @ts-ignore
+    expect(console.log.mock.calls[2]).toMatchSnapshot();
+    // @ts-ignore
+    expect(process.exit).toHaveBeenCalledWith(1);
+  });
+});
+
+test('renders logos', () => {
+  const echo: MultiCommand = {
+    name: 'echo',
+    description: 'Print a string to the terminal',
+    logo: 'FOOBAR',
+    commands: [
+      {
+        name: 'lint',
+        description: 'find common errors'
+      }
+    ]
+  };
+  jest.spyOn(console, 'log').mockImplementationOnce(() => { });
+  app(echo, { argv: ['--help'] });
+
+  // @ts-ignore
+  expect(console.log.mock.calls[0]).toMatchSnapshot();
+});
+
+test('can disable camelCase', () => {
+  const echo: Command = {
+    name: 'echo',
+    description: 'Print a string to the terminal',
+    options: [
+      {
+        name: 'lint_it',
+        description: 'find common errors',
+        type: Boolean
+      }
+    ]
+  };
+  jest.spyOn(console, 'log').mockImplementationOnce(() => { });
+  const args = app(echo, { camelCase: false, argv: ['--lint_it'] });
+
+  expect(args).toEqual(
+    expect.objectContaining({
+      lint_it: true
+    })
+  );
+});
+
+test('should display code', () => {
+  const echo: Command = {
+    name: 'echo',
+    description: 'Print a string to the terminal',
+    examples: ['echo foo', 'echo "Intense message"'],
+    footer: [
+      {
+        code: true,
+        content: `
+        \`\`\`json
+        {
+          "create": {
+            "package": {
+              "templates": [
+                {
+                  "name": "custom-js",
+                  "url": "https://github.intuit.com/me/my-template",
+                  "description": "A custom package template",
+                  "sha": "4b9c7b627307380fb31acae059f7c095d0c626b8"
+                }
+              ]
+            }
+          }
+        }
+       \`\`\``
+      }
+    ]
+  };
+
+  const log = jest.fn();
+  console.log = log;
+
+  app(echo, { argv: ['--help'] });
+
+  expect(log.mock.calls[0][0]).toMatchSnapshot();
+});
+
+describe('multi command app', () => {
+  const testCommand: Command = {
+    name: 'test',
+    description: 'test the project',
+    examples: ['test --interactive'],
+    options: [
+      {
+        name: 'interactive',
+        type: Boolean,
+        description: 'Run the application in interactive mode'
+      }
+    ]
+  };
+  const lintCommand: Command = {
+    name: 'lint',
+    description: 'lint the project',
+    examples: ['lint --fix'],
+    options: [
+      {
+        name: 'fix',
+        type: Boolean,
+        description: 'Run the application in fix mode'
+      }
+    ]
+  };
+  const scripts: MultiCommand = {
+    name: 'scripts',
+    description: 'My scripts package',
+    commands: [lintCommand, testCommand],
+    options: [
+      {
+        name: 'verbose',
+        alias: 'v',
+        description: 'Log a bunch of stuff',
+        type: Boolean
+      }
+    ]
+  };
+
+  test('help', () => {
+    jest.spyOn(console, 'log').mockImplementationOnce(() => { });
+    app(scripts, { argv: ['--help'] });
+
+    // @ts-ignore
+    expect(console.log.mock.calls[0]).toMatchSnapshot();
+  });
+
+  test('should exit when no sub command is provided', () => {
+    // @ts-ignore
+    jest.spyOn(process, 'exit').mockImplementationOnce(() => { });
+    jest.spyOn(console, 'log').mockImplementationOnce(() => { });
+    jest.spyOn(console, 'log').mockImplementationOnce(() => { });
+    app(scripts);
+
+    // @ts-ignore
+    expect(process.exit).toHaveBeenCalledWith(1);
+  });
+
+  test('use lint', () => {
+    expect(app(scripts, { argv: ['lint', '--fix'] })).toEqual(
+      expect.objectContaining({ _command: 'lint', fix: true })
+    );
+  });
+
+  test('use test', () => {
+    expect(app(scripts, { argv: ['test'] })).toEqual(
+      expect.objectContaining({ _command: 'test' })
+    );
+  });
+
+  test('errors on unknown flag', () => {
+    // @ts-ignore
+    jest.spyOn(process, 'exit').mockImplementationOnce(() => { });
+    jest.spyOn(console, 'log').mockImplementation(() => { });
+
+    app(scripts, { argv: ['--error'] });
+
+    // @ts-ignore
+    expect(console.log.mock.calls[0]).toMatchSnapshot();
+    // @ts-ignore
+    expect(console.log.mock.calls[1]).toMatchSnapshot();
+    // @ts-ignore
+    expect(process.exit).toHaveBeenCalledWith(1);
+  });
+
+  test('errors on unknown flag for sub command', () => {
+    // @ts-ignore
+    jest.spyOn(process, 'exit').mockImplementationOnce(() => { });
+    jest.spyOn(console, 'log').mockImplementation(() => { });
+
+    app(scripts, { argv: ['lint', '--error'] });
+
+    // @ts-ignore
+    expect(console.log.mock.calls[0]).toMatchSnapshot();
+    // @ts-ignore
+    expect(console.log.mock.calls[1]).toMatchSnapshot();
+    // @ts-ignore
+    expect(process.exit).toHaveBeenCalledWith(1);
+  });
+
+  test('suggests misspelled command', () => {
+    // @ts-ignore
+    jest.spyOn(process, 'exit').mockImplementationOnce(() => { });
+    jest.spyOn(console, 'log').mockImplementation(() => { });
+
+    app(scripts, { argv: ['flint'] });
+
+    // @ts-ignore
+    expect(console.log.mock.calls[0]).toMatchSnapshot();
+    // @ts-ignore
+    expect(console.log.mock.calls[1]).toMatchSnapshot();
+    // @ts-ignore
+    expect(process.exit).toHaveBeenCalledWith(1);
+  });
+
+  test('should handle curlies in descriptions', () => {
+    jest.spyOn(console, 'log').mockImplementation(() => { });
+
+    app(
+      {
+        ...scripts,
+        footer: {
+          header: 'Additional Info',
+          content:
+            'Only run this if you really need to ({ foo }) => { console.log("") }'
+        }
+      },
+      { argv: ['flint'] }
+    );
+
+    // @ts-ignore
+    expect(console.log.mock.calls[0]).toMatchSnapshot();
+    // @ts-ignore
+    expect(console.log.mock.calls[1]).toMatchSnapshot();
+  });
+
+  test('can configure error type', () => {
+    jest.spyOn(console, 'log').mockImplementation(() => { });
+
+    expect(
+      stripAnsi(app(scripts, { argv: ['flint'], error: 'object' })!.error)
+    ).toBe('Found unknown command "flint", did you mean "lint"?');
+  });
+});
+
+describe('multi command app w/multi commands', () => {
+  const testCommand: Command = {
+    name: 'test',
+    description: 'test the project',
+    examples: ['test --interactive'],
+    options: [
+      {
+        name: 'interactive',
+        type: Boolean,
+        description: 'Run the application in interactive mode'
+      }
+    ]
+  };
+  const createCommand: MultiCommand = {
+    name: 'create',
+    description: 'create a new project',
+    // examples: ['create component', 'create project'],
+    options: [
+      {
+        name: 'name',
+        type: String,
+        description: 'The name of the thing to create'
+      }
+    ],
+    commands: [
+      {
+        name: 'component',
+        description: 'create a component',
+        examples: ['create component']
+      },
+      {
+        name: 'project',
+        description: 'create a project',
+        examples: ['create project'],
+        options: [
+          {
+            name: 'cwd',
+            type: Boolean,
+            description: 'Use the current working directory'
+          }
+        ]
+      }
+    ]
+  };
+  const scripts: MultiCommand = {
+    name: 'scripts',
+    description: 'My scripts package',
+    commands: [createCommand, testCommand],
+    options: [
+      {
+        name: 'verbose',
+        alias: 'v',
+        description: 'Log a bunch of stuff',
+        type: Boolean
+      }
+    ]
+  };
+
+  test('sub-multi-command help', () => {
+    jest.spyOn(console, 'log').mockImplementationOnce(() => { });
+    app(scripts, { argv: ['create', '--help'] });
+
+    // @ts-ignore
+    expect(console.log.mock.calls[0]).toMatchSnapshot();
+  });
+
+  test('sub-multi-command sub-command help', () => {
+    jest.spyOn(console, 'log').mockImplementationOnce(() => { });
+    app(scripts, { argv: ['create', 'project', '--help'] });
+
+    // @ts-ignore
+    expect(console.log.mock.calls[0]).toMatchSnapshot();
+  });
+
+  test('sub-multi-command sub-command usage', () => {
+    expect(
+      app(scripts, { argv: ['create', 'project', '--cwd', '--name', 'foo'] })
+    ).toEqual(
+      expect.objectContaining({
+        _command: ['create', 'project'],
+        cwd: true,
+        name: 'foo'
+      })
+    );
+  });
+});
+
+describe('multi command app w/global singleton flags', () => {
+  const testCommand: Command = {
+    name: 'test',
+    description: 'test the project',
+    examples: ['test --interactive'],
+    options: [
+      {
+        name: 'interactive',
+        type: Boolean,
+        description: 'Run the application in interactive mode'
+      }
+    ]
+  };
+
+  const scripts: MultiCommand = {
+    name: 'scripts',
+    description: 'My scripts package',
+    commands: [testCommand],
+    options: [
+      {
+        name: 'version',
+        alias: 'v',
+        description: 'Get the version',
+        type: Boolean
+      }
+    ]
+  };
+
+  test('sub-multi-command help', () => {
+    expect(app(scripts, { argv: ['--version'] })).toStrictEqual({
+      version: true
+    });
+  });
+
+  test('sub-multi-command help', () => {
+    expect(app(scripts, { argv: ['test', '--version'] })!.version).toBe(true);
+  });
+});
+
+describe('multi command app sub-command groups', () => {
+  const testCommand: Command = {
+    name: 'test',
+    description: 'test the project',
+    examples: ['test --interactive'],
+    group: 'developer',
+    options: [
+      {
+        name: 'interactive',
+        type: Boolean,
+        description: 'Run the application in interactive mode'
+      }
+    ]
+  };
+
+  const styleCommand: Command = {
+    name: 'style-guide',
+    description: 'create a styleguide for the project',
+    group: 'design'
+  };
+
+  const scripts: MultiCommand = {
+    name: 'scripts',
+    description: 'My scripts package',
+    commands: [testCommand, styleCommand]
+  };
+
+  test('sub-multi-command help', () => {
+    jest.spyOn(console, 'log').mockImplementationOnce(() => { });
+    app(scripts, { argv: ['--help'] });
+
+    // @ts-ignore
+    expect(console.log.mock.calls[0]).toMatchSnapshot();
+  });
+});
